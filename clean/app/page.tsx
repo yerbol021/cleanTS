0a729b9b8a56725d8f30fc3dc1c12b23c830af54
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-28 18:20:01 -0700

    Added full logic to navbar

--- clean/app/page.tsx
+++ clean/app/page.tsx
@@ -3,10 +3,10 @@ export default function Home() {
     <>
       <div className="container mx-auto px-4">
         <h1>Hello word</h1>
-        <p>This is a content to make our page longer</p>
+        <p>Some text</p>
         <div className="w-full h-screen bg-green-300"></div>
         <p>
-          Lorem Ipsum is simply dummy text ...
+          Some more text
         </p>
       </div>
     </>
