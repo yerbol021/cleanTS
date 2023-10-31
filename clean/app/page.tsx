cc8f27bf1aadc448a4a2ab8eff127cb5862e5d26
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-10-30 20:46:59 -0700

    Started slider implementation

--- clean/app/page.tsx
+++ clean/app/page.tsx
@@ -1,9 +1,30 @@
+import React from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+
+
 export default function Home() {
   return (
     <>
+     <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
       <div className="container mx-auto px-4">
         <h1>Hello word</h1>
         <p>Some text</p>
+
         <div className="w-full h-screen bg-green-300"></div>
         <p>
           Some more text
