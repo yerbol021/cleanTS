90aba424415c7710f5302c87768565f23f0c7ebf
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-11-02 12:36:04 -0700

    Started build services page

--- clean/app/page.tsx
+++ clean/app/page.tsx
@@ -1,7 +1,4 @@
-"use client"
-import Head from "next/head";
-import BootstrapCarousel from "/Users/yerbolkuldeyev/lighthouse/cleanTS/clean/app/components/navigation/carousel/Bootstrap";
-import Slider from "./components/navigation/slider/Slider";
+
 
 export default function Home() {
   return (
@@ -9,8 +6,6 @@ export default function Home() {
       <div className="container mx-auto px-4">
         <h1>Hello word</h1>
         <p>Some text</p>
-{/* <Slider/> */}
-      
 
         <div className="w-full h-screen bg-green-300"></div>
         <p>
--- clean/app/services/page.tsx
+++ clean/app/services/page.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import ".//services.css";
 
 const Services = () => {
-  return <div>Services</div>;
+  return (
+    <div className="service-container">
+      <div className="service-details">
+        <h3>Residential Cleaning</h3>
+        <p className="first-line">
+          Free yourself from tons of housework and start getting more from your
+          weekends. We offer weekly, bi-weekly, or one-time cleaning at
+          affordable rates.
+        </p>
+        <p className="second-line">
+          Plus, we include things like ironing and appliance cleaning for the
+          same flat-fee.
+        </p>
+        <p className="third-line">
+          Also, we have many other services available such as laundry, ironing,
+          appliance cleaning, pet sitting, and more...
+        </p>
+      </div>
+      <div className="service-image">
+        <img src="/images/1.jpeg" alt="Residential Cleaning" />
+      </div>
+    </div>
+  );
 };
 
 export default Services;
