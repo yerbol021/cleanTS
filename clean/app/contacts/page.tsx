c2c00cd466c8a91b7308ed9c64a6cd8ff1c1e7a4
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-11-06 12:11:21 -0800

    Started contacts page

--- clean/app/contacts/page.tsx
+++ clean/app/contacts/page.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Contacts = () => {
-  return <div>Contacts</div>;
+  return (
+    <div className="contacts div">
+      <div className="image div">
+        <img src="/images/call us.png" alt="Call us" />
+        <img src="/images/wait us.png" alt="Wait us" />
+        <img src="/images/enjoy.png" alt="Enjoy" />
+      </div>
+    </div>
+  );
 };
 
 export default Contacts;
