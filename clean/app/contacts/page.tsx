a63922eda79ab211965227f0a0f0fab8629b8785
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-11-07 11:14:41 -0800

    Add more info to contacts

--- clean/app/contacts/page.tsx
+++ clean/app/contacts/page.tsx
@@ -6,16 +6,16 @@ const Contacts = () => {
     <div className="contacts div">
       <div className="call us">
         <img src="/images/call us.jpeg" alt="Call us" />
-        <h4>Call us and Book a date</h4>
+        <h4>Call Us and Book a date</h4>
         <h3></h3>
       </div>
       <div className="wait us">
         <img src="/images/wait us.png" alt="Wait us" />
-        <h4>Meet our cleaning team</h4>
+        <h4>Meet our cleaning Team</h4>
         <h3></h3>
       </div>
       <div className="enjoy">
-        <img src="/images/enjoy.png" alt="Enjoy" />
+        <img src="/images/enjoy.jpeg" alt="Enjoy" />
         <h4>Enjoy results</h4>
         <h3></h3>
       </div>
