a8c3eea47fd25b9a1d4ae68db5c287789c0ac9a2
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-11-06 22:51:18 -0800

    Working on contacts

--- clean/app/contacts/page.tsx
+++ clean/app/contacts/page.tsx
@@ -1,12 +1,23 @@
 import React from "react";
+import "./contacts.css";
 
 const Contacts = () => {
   return (
     <div className="contacts div">
-      <div className="image div">
-        <img src="/images/call us.png" alt="Call us" />
+      <div className="call us">
+        <img src="/images/call us.jpeg" alt="Call us" />
+        <h4>Call us and Book a date</h4>
+        <h3></h3>
+      </div>
+      <div className="wait us">
         <img src="/images/wait us.png" alt="Wait us" />
+        <h4>Meet our cleaning team</h4>
+        <h3></h3>
+      </div>
+      <div className="enjoy">
         <img src="/images/enjoy.png" alt="Enjoy" />
+        <h4>Enjoy results</h4>
+        <h3></h3>
       </div>
     </div>
   );
