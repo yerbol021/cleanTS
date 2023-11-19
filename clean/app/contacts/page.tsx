de1bcc0562a60f01abad44213302c30d4fad7df4
Yerbol Kuldeyev <yerbol.kuldeyev@gmail.com>
2023-11-18 19:22:05 -0800

    keep trying

--- clean/app/contacts/page.tsx
+++ clean/app/contacts/page.tsx
@@ -3,21 +3,28 @@ import "./contacts.css";
 
 const Contacts = () => {
   return (
-    <div className="contacts div">
-      <div className="call us">
-        <img src="/images/call us.jpeg" alt="Call us" />
-        <h4>Call Us and Book a date</h4>
-        <h3></h3>
+    <div className="main_div">
+      <div className="contacts div">
+        <div className="call us">
+          <img src="/images/call us.jpeg" alt="Call us" />
+          <h4>Call Us and Book a date</h4>
+          <h3></h3>
+        </div>
+        <div className="wait us">
+          <img src="/images/wait us.png" alt="Wait us" />
+          <h4>Meet our cleaning Team</h4>
+          <h3></h3>
+        </div>
+        <div className="enjoy">
+          <img src="/images/enjoy.jpeg" alt="Enjoy" />
+          <h4>Enjoy results</h4>
+          <h3></h3>
+        </div>
       </div>
-      <div className="wait us">
-        <img src="/images/wait us.png" alt="Wait us" />
-        <h4>Meet our cleaning Team</h4>
-        <h3></h3>
-      </div>
-      <div className="enjoy">
-        <img src="/images/enjoy.jpeg" alt="Enjoy" />
-        <h4>Enjoy results</h4>
-        <h3></h3>
+      <div className="calendly">
+        <div className="calendly form">
+
+        </div>
       </div>
     </div>
   );
--- clean/app/page.tsx
+++ clean/app/page.tsx
@@ -1,17 +1,39 @@
+import React, { useEffect } from "react";
+import Head from "next/head";
+import { useClient } from "@react/server";
 
+function Home() {
+  useClient(); // Marking the Home component as a client entry
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://assets.calendly.com/assets/external/widget.js";
+    script.async = true;
+    document.head.appendChild(script);
 
-export default function Home() {
+    return () => {
+      document.head.removeChild(script);
+    };
+  }, []);
+
   return (
-    <>
-      <div className="container mx-auto px-4">
-        <h1>Hello word</h1>
-        <p>Some text</p>
-
-        <div className="w-full h-screen bg-green-300"></div>
-        <p>
-          Some more text
-        </p>
-      </div>
-    </>
+    <div>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://assets.calendly.com/assets/external/widget.css"
+        />
+      </Head>
+      <h1>Hello, World!</h1>
+      <p>Some text.</p>
+      <div className="w-full h-screen bg-green-300"></div>
+      <p>Some more text.</p>
+      <div
+        className="calendly-inline-widget"
+        data-url="https://calendly.com/d/48p-yxk-h87?hide_gdpr_banner=1"
+        style={{ minWidth: "320px", height: "700px" }}
+      ></div>
+    </div>
   );
 }
+
+export default Home;
